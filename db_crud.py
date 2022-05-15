553f74b1357033843045d27d61738e07824fc149
Jake_YangLiang <jake.yang@faninsights.io>
2022-05-15 21:10:02 +0800

    * DB CRUD 1.從DB撈頻道網址 2.重複頻道內影片網址判斷 3.寫入影片資料

--- db_crud.py
+++ db_crud.py
@@ -1,83 +1,40 @@
 from db_config.db_setting import *
 from datetime import datetime
-
-
-# 更新頻道狀態
-def update_to_db(status_code, title, v_name, live_url):
-
-    conn, cursor = db_set()
-
-    update_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    query = {'online_status': status_code, 'stream_title': title, 'v_name': v_name, 'live_url': live_url, 'check_time': update_datetime}
-
-    sql = "update hololive_status set `online_status`=%(online_status)s, `stream_title`=%(stream_title)s, `live_url`=%(live_url)s, `check_time`=%(check_time)s where `vtuber_name`=%(v_name)s"
-
-    cursor.execute(sql, query)
-    conn.commit()
-    conn.close()
+from itertools import chain
 
 
 # 拿到頻道網址
-def select_target(v_name):
+def get_channel_url(con, cur):
+    sql = "select ch_id, ch_url from channel_info"
+    cur.execute(sql)
+    result = cur.fetchall()
 
-    conn, cursor = db_set()
+    url_dict = {}
 
-    sql = "select ch_domain_page from hololive_status where vtuber_name='%s' " % v_name
+    for res in result:
+        url_dict[res[0]] = res[1]
 
-    cursor.execute(sql)
-    result = cursor.fetchone()[0]
+    print(url_dict)
 
-    return result
-
-
-# 拿到直播狀態碼
-def check_stream(v_name):
-
-    conn, cursor = db_set()
-
-    sql = "select online_status from hololive_status where vtuber_name='%s' " % v_name
-
-    cursor.execute(sql)
-    status_code = cursor.fetchone()[0]
-    print(status_code)
-
-    return status_code
+    return url_dict
 
 
-# 拿到直播標題 網址
-def stream_url_title(v_name):
+# 防重複寫入
+def check_video_in_db(con, cur, v_url):
+    sql = "select v_url from bbq_info where v_url='%s'" % v_url
+    cur.execute(sql)
+    result = cur.fetchone()
 
-    conn, cursor = db_set()
-
-    sql = "select stream_title, live_url from hololive_status where vtuber_name='%s' " % v_name
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    stream_title = result[0][0]
-    live_url = result[0][1]
-
-    return stream_title, live_url
+    return not result
 
 
+# 寫入資料
+def insert_video(con, cur, ch_id, v_title, v_url):
+    create_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    query = {'ch_id': ch_id, 'v_title': v_title, 'v_url': v_url, 'create_datetime': create_datetime}
 
+    sql = "insert into bbq_info (`ch_id`, `v_title`, `v_url`, `create_datetime`) values ( %(ch_id)s, %(v_title)s, %(v_url)s, %(create_datetime)s)"
 
-# def go():
-#
-#     names = ['ame', 'ina', 'cali', 'gura', 'kiara']
-#
-#     for name in names:
-#
-#         print('checking:', name)
-#
-#         stream_href = select_target(name)
-#
-#         online_check, stream_title, stream_href = stream_check(name, stream_href)
-#
-#         if online_check == 'online':
-#
-#             update_to_db(1, stream_title, name, stream_href)
-#
-#         else:
-#
-#             update_to_db(0, None, name, None)
+    cur.execute(sql, query)
+    con.commit()
