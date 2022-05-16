93a6efba3c8ef65d1ee0b096bfb4be1ea45a627b
Jake_YangLiang <jake.yang@faninsights.io>
2022-05-16 23:41:36 +0800

    *add db

---
+++ db_config/db_set.py
@@ -0,0 +1,23 @@
+import pymysql
+
+
+def db_set():
+    db_setting = {
+
+        "host": "34.81.126.51",
+        "port": 3306,
+        "user": "hololive-bbq",
+        "password": "kK=LillE'.8o~;vJ",
+        "db": "holo_bot"
+    }
+
+    connect = pymysql.connect(**db_setting)
+
+    cursor_ = connect.cursor()
+
+    return connect, cursor_
+
+
+if __name__ == "__main__":
+    conn, cursor = db_set()
+    conn.close()
